350107a14ef73c163a9bc03e9b4ff5a9b7e097ad
Cristina Popa <cristinapopa@Cristinas-MacBook-Air.local>
2025-01-03 21:31:58 +0200

    second stage

--- app/main.ts
+++ app/main.ts
@@ -1,13 +1,12 @@
 import * as net from "net";
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+// console.log("Logs from your program will appear here!");
 
-//Uncomment this to pass the first stage
+// Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on("close", () => {
-    socket.end();
-  });
+  socket.write('HTTP/1.0 200 OK\r\n\r\n');
+  socket.end();
 });
 
 server.listen(4221, "localhost");
