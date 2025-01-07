4cdaadadb56f61afa0fc84f9c5dc705d533d43c4
Cristina Popa <cristinapopa@Cristinas-MacBook-Air.local>
2025-01-07 11:56:54 +0200

    refactor

--- app/main.ts
+++ app/main.ts
@@ -1,40 +1,86 @@
 import * as net from "net";
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-// console.log("Logs from your program will appear here!");
 
-// Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  // socket.write('HTTP/1.1 200 OK\r\n\r\n');
-
-  // socket.on('connection', (stream) => {
-  //   console.log('somebody connected');
-  // })
+  console.log('New connection established...');
 
   socket.on('data', (data) => {
-    
-    const reqString = data.toString();
-
-    const [requestLine] = reqString.split('\r\n');
-    const [method, path, version] = reqString.split(' ');
-
-    if (path === '/') {
-      socket.write('HTTP/1.1 200 OK\r\n\r\n');
-    } else if (/^\/echo/.test(path)) {
-      const [,words] = path.split('/echo/');
-      socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${words.length}\r\n\r\n${words}`);
-    } else if (/^\/user-agent/.test(path)) {
-      const userAgent = reqString.split('User-Agent: ')[1].split('\r\n')[0];
-      socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`);
-    } else {
-      socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
+    try {
+      handleRequest(data.toString(), socket);
+    } catch (error) {
+      console.log("Error handling request: ", error);
+      socket.write("HTTP/1.1 500 Internal Server Error\r\n\r\n");
     }
   });
-
+    
   socket.on('end', () => {
-    console.log('Connection closed');
+    console.log('Connection closed!');
   });
+
+  socket.on('error', (err) => {
+    console.log('Socket error:', err);
+  })
 
 });
 
+function handleRequest (request: string, socket: net.Socket) {
+  const [requestLine, ...headers] = request.split("\r\n");
+  const [method, path, version] = requestLine.split(" ");
+  
+  if (path === '/') {
+    sendResponse(socket, 200, "OK", "text/plain", "");
+    return;
+  }
+
+  const indexPath = path.split("/").filter(Boolean);
+  const [indexRoute, ...parameters] = indexPath;
+  console.log('index route: ', indexRoute);
+
+  if(!method || !version) {
+    socket.write("HTTP/1.1 400 Bad Request\r\n\r\n");
+    return;
+  }
+
+  switch (indexRoute) {
+    case 'echo':
+      const body = decodeURIComponent(path.replace("/echo/", ""));
+      sendResponse(socket, 200, "OK", "text/plain", body);
+      break;
+    case 'user-agent':
+      const userAgent = getHeaderValue(headers, "User-Agent");
+      userAgent ? 
+      sendResponse(socket, 200, "OK", "text/plain", userAgent)
+      : sendResponse(socket, 400, "Bad Request", "text/plain", "User-Agent not found");
+      break;
+    default:
+      sendResponse(socket, 404, "Not Found", "text/plain", "");
+      break;
+
+  }
+}
+
 server.listen(4221, "localhost");
+
+function sendResponse(
+  socket: net.Socket,
+  statusCode: number,
+  statusMessage: string,
+  contentType: string,
+  body: string
+) {
+  const headers = [
+    `HTTP/1.1 ${statusCode} ${statusMessage}`,
+    `Content-Type: ${contentType}`,
+    `Content-Length: ${Buffer.byteLength(body)}`,
+    "",
+    body,
+  ];
+
+  socket.write(headers.join("\r\n"));
+}
+
+      
+function getHeaderValue(headers: string[], headerName: string): string | null {
+  const header = headers.find((h) => h.toLowerCase().startsWith(headerName.toLowerCase() + ":"));
+  return header ? header.split(":")[1].trim() : null;
+}
